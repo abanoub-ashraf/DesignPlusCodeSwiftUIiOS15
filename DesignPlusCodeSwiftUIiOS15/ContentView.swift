a1537fb886eeca30daace8c345f5f8433bda79b2
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2022-11-19 22:01:46 +0200

    assets + first card ui

--- DesignPlusCodeSwiftUIiOS15/ContentView.swift
+++ DesignPlusCodeSwiftUIiOS15/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8) {
+            Spacer()
+            
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26, height: 26)
+                .cornerRadius(20)
+            
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("20 Sections - 3 Hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and more")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        .padding(.all, 20)
+        .frame(height: 350)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .cornerRadius(30)
+        .shadow(radius: 20)
     }
 }
 
