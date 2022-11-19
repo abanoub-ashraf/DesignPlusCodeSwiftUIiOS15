db7a980f42c54be789e75443c611ae93e026187c
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2022-11-19 20:32:52 +0200

    prepare the app

---
+++ DesignPlusCodeSwiftUIiOS15/DesignPlusCodeSwiftUIiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPlusCodeSwiftUIiOS15App.swift
+//  DesignPlusCodeSwiftUIiOS15
+//
+//  Created by Abanoub Ashraf on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignPlusCodeSwiftUIiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
